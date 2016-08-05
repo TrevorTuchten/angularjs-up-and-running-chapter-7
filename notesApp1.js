abc7a4fe07dedb81e11e9913d38d7160634c9294
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-05 16:07:43 -0700

    Rewrite

--- notesApp1.js
+++ notesApp1.js
@@ -2,8 +2,7 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('notesApp1', [])
-  .factory('ItemService', [function() {
+angular.module('notesApp1', []).factory('ItemService', [function() {
     var items = [
       {id: 1, label: 'Item 0'},
       {id: 2, label: 'Item 1'}
@@ -16,8 +15,7 @@ angular.module('notesApp1', [])
         items.push(item);
       }
     };
-  }])
-  .controller('ItemCtrl', ['ItemService', function(ItemService) {
+}]).controller('ItemCtrl', ['ItemService', function(ItemService) {
     var self = this;
     self.items = ItemService.list();
-  }]);
+    }]);
