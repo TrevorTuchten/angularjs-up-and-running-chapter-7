af78a01a369f5518765ef17bb5f7edfc948344d8
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-05 16:33:30 -0700

    Rewrite

--- serverApp.js
+++ serverApp.js
@@ -2,8 +2,7 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('serverApp', [])
-  .controller('MainCtrl', ['$http', function($http) {
+angular.module('serverApp', []).controller('MainCtrl', ['$http', function($http) {
     var self = this;
     self.items = [];
     self.errorMessage = '';
@@ -13,4 +12,4 @@ angular.module('serverApp', [])
     }, function(errResponse) {
       self.errorMessage = errResponse.data.msg;
     });
-  }]);
+}]);
