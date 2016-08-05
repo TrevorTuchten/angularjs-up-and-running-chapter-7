3dfe26f07ea0799251743edc4a363e4dfcee0ee0
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-05 15:57:04 -0700

    Rewrite

--- simpleCtrl1.js
+++ simpleCtrl1.js
@@ -2,10 +2,9 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('simpleCtrl1App', [])
-  .controller('SimpleCtrl', ['$location', function($location) {
+angular.module('simpleCtrl1App', []).controller('SimpleCtrl', ['$location', function($location) {
     var self = this;
     self.navigate = function() {
       $location.path('/some/where/else');
     };
-  }]);
+}]);
