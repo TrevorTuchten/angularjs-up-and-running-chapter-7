15def9393811f1bf14c8cbdbd7398a78adc06e01
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-05 16:21:02 -0700

    rewrite

--- notesApp1-mocks.js
+++ notesApp1-mocks.js
@@ -2,8 +2,7 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('notesApp1Mocks', [])
-  .factory('ItemService', [function() {
+angular.module('notesApp1Mocks', []).factory('ItemService', [function() {
     return {
       list: function() {
         return [{id: 1, label: 'Mock'}];
