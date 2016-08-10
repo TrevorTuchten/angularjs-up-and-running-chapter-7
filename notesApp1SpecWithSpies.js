4d6e8497d1aa16287901beb16b1afbcc96914219
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-10 06:10:08 -0700

    Rewrite

--- notesApp1SpecWithSpies.js
+++ notesApp1SpecWithSpies.js
@@ -2,23 +2,22 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-describe('ItemCtrl with spies', function() {
-  beforeEach(module('notesApp1'));
+describe('ItemCtrl with spies', function () {
+    beforeEach(module('notesApp1'));
 
-  var ctrl, itemService;
+    var ctrl, itemService;
+    beforeEach(inject(function ($controller, ItemService) {
+        spyOn(ItemService, 'list').and.callThrough();
+        itemService = ItemService;
+        ctrl = $controller('ItemCtrl');
+    }));
 
-  beforeEach(inject(function($controller, ItemService) {
-    spyOn(ItemService, 'list').and.callThrough();
-    itemService = ItemService;
-    ctrl = $controller('ItemCtrl');
-  }));
-
-  it('should load mocked out items', function() {
-    expect(itemService.list).toHaveBeenCalled();
-    expect(itemService.list.calls.count()).toEqual(1);
-    expect(ctrl.items).toEqual([
-      {id: 1, label: 'Item 0'},
-      {id: 2, label: 'Item 1'}
-    ]);
-  });
+    it('should load mocked out items', function () {
+        expect(itemService.list).toHaveBeenCalled();
+        expect(itemService.list.calls.count()).toEqual(1);
+        expect(ctrl.items).toEqual([
+            {id: 1, label: 'Item 0'},
+            {id: 2, label: 'Item 1'}
+        ]);
+    });
 });
--- serverApp.js
+++ serverApp.js
@@ -2,14 +2,16 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('serverApp', []).controller('MainCtrl', ['$http', function($http) {
-    var self = this;
-    self.items = [];
-    self.errorMessage = '';
+angular.module('serverApp', [])
+    .controller('MainCtrl', ['$http', function ($http) {
+        var self = this;
+        self.items = [];
+        self.errorMessage = '';
 
-    $http.get('/api/note').then(function(response) {
-      self.items = response.data;
-    }, function(errResponse) {
-      self.errorMessage = errResponse.data.msg;
-    });
-}]);
+        $http.get('/api/note')
+            .then(function (response) {
+                self.items = response.data;
+            }, function (errResponse) {
+                self.errorMessage = errResponse.data.msg;
+            });
+    }]);
