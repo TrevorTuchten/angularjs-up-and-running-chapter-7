1621ca956c5e97bcbf068a3f798f1901e8f62509
Trevor Tuchten <trevor.tuchten@tntgenesis.com>
2016-08-10 05:06:07 -0700

    Rewrite

--- simpleCtrl1.js
+++ simpleCtrl1.js
@@ -2,9 +2,10 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('simpleCtrl1App', []).controller('SimpleCtrl', ['$location', function($location) {
-    var self = this;
-    self.navigate = function() {
-      $location.path('/some/where/else');
-    };
-}]);
+angular.module('simpleCtrl1App', [])
+    .controller('SimpleCtrl', ['$location', function ($location) {
+        var self = this;
+        self.navigate = function () {
+            $location.path('/some/where/else');
+        };
+    }]);
--- simpleCtrl2.js
+++ simpleCtrl2.js
@@ -2,13 +2,13 @@
  * Created by tntdi_000 on 8/5/2016.
  */
 
-angular.module('simpleCtrl2App', []).controller('SimpleCtrl2', ['$location', '$window',
-    function($location, $window) {
-      var self = this;
-      self.navigate1 = function() {
-        $location.path('/some/where');
-      };
-      self.navigate2 = function() {
-        $location.path('/some/where/else');
-      };
+angular.module('simpleCtrl2App', [])
+    .controller('SimpleCtrl2', ['$location', '$window', function ($location, $window) {
+        var self = this;
+        self.navigate1 = function () {
+            $location.path('/some/where');
+        };
+        self.navigate2 = function () {
+            $location.path('/some/where/else');
+        };
     }]);
